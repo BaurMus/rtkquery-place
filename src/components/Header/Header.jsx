9a394798bfafa2426696860d7c21224eb45bd0fd
BaurMus <musilimov_b@mail.ru>
2024-03-19 12:17:27 +0500

    added hook useFavorites for optimization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import { TbLocationHeart } from "react-icons/tb";
 import styles from './Header.module.css';
 import { useSelector } from "react-redux";
+import { useFavorites } from "../../store/hooks/useFavorites";
 
 export default function Header() {
-  const favorites = useSelector(state => state.favorites);
+  const favorites = useFavorites();
 
   return (
     <div className={styles.header}>
--- src/components/place-item/PlaceItem.jsx
+++ src/components/place-item/PlaceItem.jsx
@@ -1,16 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
 import styles from './PlaceItem.module.css';
-import { actions } from '../../store/favorites/favorites.slices';
+import { useActions } from '../../store/hooks/useActions';
+import { useFavorites } from '../../store/hooks/useFavorites';
 
 export default function PlaceItem({place}) {
-  const dispatch = useDispatch();
-  const favorites = useSelector(state => state.favorites);
+  const {toggleFavorites} = useActions();
+  const favorites = useFavorites();
   const isExists = favorites.some(p => p.id === place.id)
 
   return (
     <div className={styles.place}>
       <h2>{place.name}</h2>
-      <button onClick={() => dispatch(actions.toggleFavorites(place))}>
+      <button onClick={() => toggleFavorites(place)}>
         {isExists ? 'Remove from' : 'Add to'} favorites
       </button>
     </div>
